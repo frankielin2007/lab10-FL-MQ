ad38801a3ccee9a989e97e9d2b7b2d2bc267fb98
frankielin2007 <frankielin2007@gmail.com>
2025-11-10 22:32:54 -0500

    restart

--- calculator.py
+++ calculator.py
@@ -1,17 +1,11 @@
-import math
-def add(a,b):
-    return a + b
-def sub(a,b):
-    return a - b
-def mul(a, b):
-    return a * b
-def div(a,b):
-    if b == 0:
-        raise ZeroDivisionError("Cannot divide by zero!")
-    return a / b
-def log(a,b):
-    if a <= 0 or b <= 0 or b == 1:
-        raise ValueError("Logarithm arguments must satisfy: a > 0, b > 0, b != 1")
-    return math.log(a,b)
-def exp(a, b):
-    return a ** b
+"""
+calculator.py
+- Defines functions used to create a simple calculator
+
+One function per operation, in order.
+"""
+# First example
+def add(a, b): 
+    pass
+
+
