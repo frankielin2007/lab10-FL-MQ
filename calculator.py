384c9adac7a83d0c2fe63010e700cfc886eb38e4
frankielin2007 <frankielin2007@gmail.com>
2025-11-10 22:23:59 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,23 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
-
-
+import math
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a,b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero!")
+    return a / b
+def log(a,b):
+    if a <= 0 or b <= 0 or b == 1:
+        raise ValueError("Logarithm arguments must satisfy: a > 0, b > 0, b != 1")
+    return math.log(a,b)
+def exp(a, b):
+    return a ** b
+def sqaure_root(a):
+    if a < 0:
+        raise ValueError("Cannot compute square root of negative number!")
+    return math.sqrt(a)
+def hypotenuse(a, b):
+    return math.hypot(a, b)
